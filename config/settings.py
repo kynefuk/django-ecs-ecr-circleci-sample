b9bb8ab69b1f62dc6dd4459dda5b9a0876f7b171
kynefuk <atsuminamihira@gmail.com>
2019-11-09 14:38:31 +0900

    aws-ecr&aws-ecs version up

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, '.env'))
 
